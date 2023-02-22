76880e6f877fd97ee60b098e5c1c0ced9b8f1bea
henry75652 <78725691+henry75652@users.noreply.github.com>
2023-02-22 15:10:55 +0800

    Create jetson_cpu.py

---
+++ data_clooect/jetson_cpu.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# This file is part of the jetson_stats package (https://github.com/rbonghi/jetson_stats or http://rnext.it).
+# Copyright (c) 2019-2023 Raffaello Bonghi.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from jtop import jtop, JtopException
+import csv
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Simple jtop logger')
+    # Standard file to store the logs
+    parser.add_argument('--file', action="store", dest="file", default="log.csv")
+    args = parser.parse_args()
+
+    print("Simple jtop logger")
+    print("Saving log on {file}".format(file=args.file))
+
+    try:
+        with jtop() as jetson:
+            # Make csv file and setup csv
+            with open("log", 'w+') as f:
+                stats = jetson.stats
+                print(type(stats))
+                # Initialize cws writer
+                f.write(f"""
+time = {stats["time"]}
+power cur = {stats["power cur"]}
+power avg = {stats["power avg"]}
+==================================
+                """)                
+                
+                # Start loop
+                while jetson.ok():
+                    stats = jetson.stats
+                    # Write row
+                    
+                    f.write(f"""
+time = {stats["time"]}
+power cur = {stats["power cur"]}
+power avg = {stats["power avg"]}
+==================================
+                """)                
+                    print("Log at {time}".format(time=stats['time']))
+    except JtopException as e:
+        print(e)
+    except KeyboardInterrupt:
+        print("Closed with CTRL-C")
+    except IOError:
+        print("I/O error")
+# EOF
